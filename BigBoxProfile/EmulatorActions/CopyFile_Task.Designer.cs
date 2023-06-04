f51d7d5963dc893ce263dc6a4c20054c58f5d924
nixx <nixx@lebrazi.com>
2023-06-04 17:23:46 +0200

    GUI improvment on CopyFile module

--- BigBoxProfile/EmulatorActions/CopyFile_Task.Designer.cs
+++ BigBoxProfile/EmulatorActions/CopyFile_Task.Designer.cs
@@ -31,20 +31,24 @@
 			this.progressBar1 = new System.Windows.Forms.ProgressBar();
 			this.lbl_progress = new System.Windows.Forms.Label();
 			this.lbl_progressETA = new System.Windows.Forms.Label();
+			this.lbl_source = new System.Windows.Forms.Label();
+			this.lbl_dest = new System.Windows.Forms.Label();
+			this.lbl_file = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(110, 157);
+			this.progressBar1.Location = new System.Drawing.Point(79, 176);
 			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(819, 36);
+			this.progressBar1.Size = new System.Drawing.Size(996, 36);
 			this.progressBar1.TabIndex = 0;
 			// 
 			// lbl_progress
 			// 
 			this.lbl_progress.AutoSize = true;
 			this.lbl_progress.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lbl_progress.Location = new System.Drawing.Point(334, 216);
+			this.lbl_progress.ForeColor = System.Drawing.Color.White;
+			this.lbl_progress.Location = new System.Drawing.Point(72, 256);
 			this.lbl_progress.Name = "lbl_progress";
 			this.lbl_progress.Size = new System.Drawing.Size(109, 39);
 			this.lbl_progress.TabIndex = 1;
@@ -54,20 +58,59 @@
 			// 
 			this.lbl_progressETA.AutoSize = true;
 			this.lbl_progressETA.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lbl_progressETA.Location = new System.Drawing.Point(334, 267);
+			this.lbl_progressETA.ForeColor = System.Drawing.Color.White;
+			this.lbl_progressETA.Location = new System.Drawing.Point(72, 310);
 			this.lbl_progressETA.Name = "lbl_progressETA";
 			this.lbl_progressETA.Size = new System.Drawing.Size(109, 39);
 			this.lbl_progressETA.TabIndex = 2;
 			this.lbl_progressETA.Text = "label1";
 			// 
+			// lbl_source
+			// 
+			this.lbl_source.AutoSize = true;
+			this.lbl_source.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_source.ForeColor = System.Drawing.Color.White;
+			this.lbl_source.Location = new System.Drawing.Point(75, 76);
+			this.lbl_source.Name = "lbl_source";
+			this.lbl_source.Size = new System.Drawing.Size(60, 24);
+			this.lbl_source.TabIndex = 3;
+			this.lbl_source.Text = "label1";
+			// 
+			// lbl_dest
+			// 
+			this.lbl_dest.AutoSize = true;
+			this.lbl_dest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_dest.ForeColor = System.Drawing.Color.White;
+			this.lbl_dest.Location = new System.Drawing.Point(75, 112);
+			this.lbl_dest.Name = "lbl_dest";
+			this.lbl_dest.Size = new System.Drawing.Size(60, 24);
+			this.lbl_dest.TabIndex = 4;
+			this.lbl_dest.Text = "label2";
+			// 
+			// lbl_file
+			// 
+			this.lbl_file.AutoSize = true;
+			this.lbl_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_file.ForeColor = System.Drawing.Color.White;
+			this.lbl_file.Location = new System.Drawing.Point(75, 22);
+			this.lbl_file.Name = "lbl_file";
+			this.lbl_file.Size = new System.Drawing.Size(109, 39);
+			this.lbl_file.TabIndex = 5;
+			this.lbl_file.Text = "label1";
+			// 
 			// CopyFile_Task
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1208, 475);
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(1166, 385);
+			this.Controls.Add(this.lbl_file);
+			this.Controls.Add(this.lbl_dest);
+			this.Controls.Add(this.lbl_source);
 			this.Controls.Add(this.lbl_progressETA);
 			this.Controls.Add(this.lbl_progress);
 			this.Controls.Add(this.progressBar1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "CopyFile_Task";
 			this.Text = "CopyFile_Task";
 			this.Load += new System.EventHandler(this.CopyFile_Task_Load);
@@ -81,5 +124,8 @@
 		private System.Windows.Forms.ProgressBar progressBar1;
 		private System.Windows.Forms.Label lbl_progress;
 		private System.Windows.Forms.Label lbl_progressETA;
+		private System.Windows.Forms.Label lbl_source;
+		private System.Windows.Forms.Label lbl_dest;
+		private System.Windows.Forms.Label lbl_file;
 	}
 }
--- BigBoxProfile/EmulatorActions/CopyFile_Task.cs
+++ BigBoxProfile/EmulatorActions/CopyFile_Task.cs
@@ -16,18 +16,68 @@ namespace BigBoxProfile.EmulatorActions
 	{
 		string inPath;
 		string outPath;
+		private DateTime startTime;
+		private bool copyCompleted = false;
+		private bool copyStart = false;
+		long fileSize = 0;
+		long totalBytesRead = 0;
+
+
 		public CopyFile_Task(string inPath, string outPath)
 		{
 			this.inPath = inPath;
 			this.outPath = outPath;
+			copyCompleted = false;
+			copyStart = false;
+			fileSize = 0;
+			totalBytesRead = 0;
 			InitializeComponent();
+			lbl_file.Text = Path.GetFileName(inPath);
+			lbl_source.Text = $"Source : {inPath}";
+			lbl_dest.Text = $"Destination : {outPath}";
 		}
 
 
 
 		private void CopyFile_Task_Load(object sender, EventArgs e)
 		{
-			lbl_progress.Text = "Salut !";
+			lbl_progress.Text = "";
+			lbl_progressETA.Text = "";
+			
+
+			// Vérifier si les chemins d'entrée et de sortie sont valides
+			if (!File.Exists(inPath))
+			{
+				MessageBox.Show("Le fichier d'entrée n'existe pas.");
+				return;
+			}
+
+			if (File.Exists(outPath))
+			{
+				var fileSizeIn = new FileInfo(inPath).Length;
+				var fileSizeOut = new FileInfo(outPath).Length;
+				if(fileSizeIn == fileSizeOut)
+				{
+					Invoke(new Action(() =>
+					{
+						Close();
+					}));
+					return;
+				}
+				else
+				{
+					File.Delete(outPath);
+				}
+			}
+			Thread displayThread = new Thread(UpdateDisplay);
+			displayThread.Start();
+			// Démarrer le thread de copie de fichier
+			Thread copyThread = new Thread(() => CopyFileWithProgress(inPath, outPath));
+			copyThread.Start();
+
+			// Démarrer le thread d'affichage
+
+
 			//CopyFileWithProgress(inPath, outPath);
 			/*
 			// Démarrer une tâche en arrière-plan pour augmenter le pourcentage
@@ -50,6 +100,7 @@ namespace BigBoxProfile.EmulatorActions
 			});
 			*/
 
+			/*
 			ThreadPool.QueueUserWorkItem((state) =>
 			{
 				long fileSize = new FileInfo(inPath).Length;
@@ -94,46 +145,111 @@ namespace BigBoxProfile.EmulatorActions
 					}
 				}
 			});
+			*/
+
+
 		}
 
-		public void CopyFileWithProgress(string inPath, string outPath)
+		private void CopyFileWithProgress(string inPath, string outPath)
 		{
-			// Obtenir la taille du fichier
-			long fileSize = new FileInfo(inPath).Length;
-			long totalBytesRead = 0;
+			fileSize = new FileInfo(inPath).Length;
+			totalBytesRead = 0;
 
-			using (FileStream inFile = new FileStream(inPath, FileMode.Open, FileAccess.Read))
+			startTime = DateTime.Now;
+			try
 			{
-				using (FileStream outFile = new FileStream(outPath, FileMode.Create, FileAccess.Write))
+				using (FileStream inFile = new FileStream(inPath, FileMode.Open, FileAccess.Read))
 				{
-					byte[] buffer = new byte[4096]; // Taille du tampon pour la copie du fichier
-					int bytesRead;
-
-					while ((bytesRead = inFile.Read(buffer, 0, buffer.Length)) > 0)
+					using (FileStream outFile = new FileStream(outPath, FileMode.Create, FileAccess.Write))
 					{
-						outFile.Write(buffer, 0, bytesRead);
-						totalBytesRead += bytesRead;
+						copyStart = true;
+						byte[] buffer = new byte[40096];
+						int bytesRead;
 
-						// Calculer la progression en pourcentage
-						int progress = (int)((totalBytesRead * 100) / fileSize);
-
-						// Mettre à jour la ProgressBar sur le thread de l'interface utilisateur
-						progressBar1.Invoke(new Action(() =>
+						while ((bytesRead = inFile.Read(buffer, 0, buffer.Length)) > 0)
 						{
-							progressBar1.Value = progress;
-						}));
-
-						// Mettre à jour le label avec le pourcentage de progression et le nombre de Mo copiés
-						double copiedSizeMB = (double)totalBytesRead / (1024 * 1024);
-						double totalSizeMB = (double)fileSize / (1024 * 1024);
-						lbl_progress.Invoke(new Action(() =>
-						{
-							lbl_progress.Text = $"Progression : {progress}% ({copiedSizeMB:F2} MB / {totalSizeMB:F2} MB)";
-						}));
+							outFile.Write(buffer, 0, bytesRead);
+							totalBytesRead += bytesRead;
+						}
 					}
 				}
+				copyCompleted = true;
 			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+
 		}
+
+		private void UpdateDisplay()
+		{
+			
+			while (true)
+			{
+				if(copyStart == false || fileSize == 0)
+				{
+					Thread.Sleep(50);
+					continue;
+				}
+				// Vérifier si la copie est terminée
+				if (copyCompleted)
+				{
+					Thread.Sleep(500);
+					Invoke(new Action(() =>
+					{
+						Close();
+					}));
+					break;
+				}
+				
+				
+
+				// Calculer la progression en pourcentage
+				int progress = (int)((progressBar1.Maximum * totalBytesRead) / fileSize);
+				
+				// Mettre à jour la ProgressBar et le label sur le thread de l'interface utilisateur
+				Invoke(new Action(() =>
+				{
+					progressBar1.Value = progress;
+					double elapsedSeconds = (DateTime.Now - startTime).TotalSeconds;
+					if (progress > 0)
+					{
+						
+						double remainingSeconds = (elapsedSeconds / progress) * (100 - progress);
+						TimeSpan remainingTime = TimeSpan.FromSeconds(remainingSeconds);
+						lbl_progressETA.Text = $"Temps restant : {remainingTime.Minutes:D2}:{remainingTime.Seconds:D2}";
+
+					}
+					else
+					{
+						lbl_progressETA.Text = "";
+					}
+
+					double transferSpeed = 0;
+					if (elapsedSeconds > 0)
+					{
+						transferSpeed = totalBytesRead / (1024 * 1024 * elapsedSeconds); // Convertir les octets en Mo
+
+					}
+					
+					
+
+
+					double copiedSizeMB = (double)totalBytesRead / (1024 * 1024);
+					double totalSizeMB = (double)fileSize / (1024 * 1024);
+					lbl_progress.Text = $"Progression : {progress}% ({copiedSizeMB:F2} MB / {totalSizeMB:F2} MB) {transferSpeed:F2} Mo/s";
+
+
+				}));
+
+				
+				Thread.Sleep(100); // Attendre 500 millisecondes avant la prochaine mise à jour de l'affichage
+			}
+			
+		}
+
 
 
 
